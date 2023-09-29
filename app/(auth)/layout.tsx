f5b1428e8fbdedb0f976695be73e2daee32acf09
Aryan Mehta <aryannmehta@gmail.com>
2023-09-29 17:33:48 +0530

    Add: Configure middleware, sign-in sign-out, topbar

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,10 +4,6 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
-import Topbar from "@/components/shared/Topbar";
-import Bottombar from "@/components/shared/Bottombar";
-import LeftSidebar from "@/components/shared/LeftSidebar";
-import RightSidebar from "@/components/shared/RightSidebar";
 import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -29,22 +25,7 @@ export default function RootLayout({
       // }}
     >
       <html lang='en'>
-        <body className={inter.className}>
-          <Topbar />
-
-          <main className='flex flex-row'>
-            <LeftSidebar />
-            <section className='main-container'>
-              <div className='w-full max-w-4xl'>
-                {children}
-              </div>
-            </section>
-            {/* @ts-ignore */}
-            <RightSidebar />
-          </main>
-
-          <Bottombar />
-        </body>
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
       </html>
     </ClerkProvider>
   );
