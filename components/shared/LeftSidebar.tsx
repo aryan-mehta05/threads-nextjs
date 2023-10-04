aa9349a931d216b5b213654bb4cfc7c1a0055b00
Aryan Mehta <aryannmehta@gmail.com>
2023-10-04 16:04:50 +0530

    Add: Profile view & tabs (personal/all)

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -3,19 +3,22 @@
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
-import { SignedIn, SignOutButton } from "@clerk/nextjs";
+import { SignedIn, SignOutButton, useAuth } from "@clerk/nextjs";
 
 import { sidebarLinks } from "@/constants";
 
 function LeftSidebar() {
   const router = useRouter();
   const pathname = usePathname();
+  const { userId } = useAuth();
 
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
         {sidebarLinks.map((link) => {
           const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+
+          if (link.route === '/profile') link.route = `${link.route}/${userId}`;
           
           return (
             <Link 
@@ -41,7 +44,7 @@ function LeftSidebar() {
           <SignOutButton signOutCallback={() => router.push('/sign-in')}>
             <div className="flex cursor-pointer gap-4 p-4">
               <Image
-                src="assets/logout.svg"
+                src="/assets/logout.svg"
                 alt="logout"
                 width={24}
                 height={24}
